7f179d7da6cb154f11e6e2659720cf41e3577cbe
GreatJang <1wkdwnsgur@gmail.com>
2025-07-19 01:22:52 +0900

    ArgoCD test

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GitOps 기반 EKS CI/CD 프로젝트에 오신 것을 환영합니다!"
+    return "GitOps 기반 EKS CI/CD 프로젝트에 오신 것을 환영합니다! ArgoCD 테스트중"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
